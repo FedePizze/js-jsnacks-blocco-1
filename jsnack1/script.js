63709e0567a2e5bc775a85c330a5b1711e74332c
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-27 17:48:13 +0100

    script jsnack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,7 +1,5 @@
 /*
 1 - Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-2 - Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
-3 - Calcola la somma e la media dei primi 10 numeri.
 */
 
 let numero = '';
@@ -10,16 +8,9 @@ let cubo = '';
 
 for (i = 0; i <= 9; i++){
     numero = parseInt(prompt('Inserisci numero'));
+    li += `<li id="num">${numero}</li>`;
+
     
-
-    if (i >= 2 && i <= 5) {
-        cubo = (Math.pow(numero, numero) * numero);
-        li += `<li>${cubo}</li>`
-    } else {
-        li += `<li>${numero}</li>`
-    }
-
     let block1 = document.getElementById('block1');
     block1.innerHTML = li;
 }
-
