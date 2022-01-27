706f2d83ba582250e8d7de3abf78d95b31b904b2
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-27 17:48:22 +0100

    script  jsnack 1

---
+++ jsnack2/script.js
@@ -0,0 +1,20 @@
+/*
+2 - Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
+*/
+
+let tot = '';
+let numero = '';
+let li = '';
+let cubo = '';
+
+tot = parseInt(prompt('Inserisci numero'));
+
+for (i = 1; i <= tot; i++){
+    numero = i;
+    cubo = (Math.pow(numero, numero) * numero);
+    li += `<li id="num">${cubo}</li>`
+    
+
+    let block1 = document.getElementById('block1');
+    block1.innerHTML = li;
+}
